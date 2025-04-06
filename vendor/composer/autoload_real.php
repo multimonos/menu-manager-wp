38433ff959aa1559d207a10127751e615ef660b4
Craig Hopgood <searaig@gmail.com>
2025-04-06 13:42:36 -0400

    install eloquent

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInita239904dbe6528ff8319b8822d2e77b2
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInita239904dbe6528ff8319b8822d2e77b2', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInita239904dbe6528ff8319b8822d2e77b2', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInita239904dbe6528ff8319b8822d2e77b2
         call_user_func(\Composer\Autoload\ComposerStaticInita239904dbe6528ff8319b8822d2e77b2::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita239904dbe6528ff8319b8822d2e77b2::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
+        }
 
         return $loader;
     }
