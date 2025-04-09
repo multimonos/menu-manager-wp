84d0bc2991605ec31bcf77abc08006c55764a18c
Craig Hopgood <searaig@gmail.com>
2025-04-09 09:11:06 -0400

    refactor create to separate action

--- lib/MenuManager/Actions/ImportExecuteAction.php
+++ lib/MenuManager/Actions/ImportExecuteAction.php
@@ -4,9 +4,7 @@ namespace MenuManager\Actions;
 
 use Illuminate\Support\Collection;
 use MenuManager\Database\db;
-use MenuManager\Database\Model\Impex;
 use MenuManager\Database\Model\Job;
-use MenuManager\Database\Model\MenuFactory;
 use MenuManager\Database\PostType\MenuPost;
 
 class ImportExecuteAction {
@@ -40,7 +38,8 @@ class ImportExecuteAction {
             $menu = MenuPost::find( $menu_id );
 
             if ( $menu === null ) {
-                $this->create( $menu_id, $rows );
+                $action = new MenuCreateAction();
+                $action->run( $menu_id, $rows );
             } else {
                 $this->update( $menu, $rows );
             }
@@ -59,99 +58,6 @@ class ImportExecuteAction {
 
     }
 
-    protected function create( $menu_id, Collection $items ): bool {
-
-        db::load()->getConnection()->transaction( function () use ( $menu_id, $items ) {
-
-            // MENU
-            $menu = MenuPost::save( ['post_title' => $menu_id, 'post_name' => $menu_id] );
-
-            if ( ! $menu instanceof \WP_Post ) {
-                return false;
-            }
-
-            // ROOT
-            $root = MenuFactory::createRootNode( $menu );
-
-            // PAGE
-            $pages = $items->groupBy( 'page' );
-
-            $pages->each( function ( Collection $rows, string $page_slug ) use ( $menu, $root ) {
-
-                // PAGE
-                $page = MenuFactory::createPageNode( $menu, $root, $page_slug );
-
-                // ALL ITEMS
-                $parents = [
-                    0 => $page,
-                ];
-                $level = 0;
-                $cnt = 0;
-
-                while ( $cnt < $rows->count() ) {
-                    $row = $rows[$cnt];
-
-                    if ( Impex::isCategoryType( $row ) ) {
-                        // CATEGORY
-                        $node = MenuFactory::createCategoryNode( $menu, $row );
-                        $root->fixTree();
-
-                        // get parent for this level
-                        $parent = $parents[$node->level] ?? null;
-
-                        // guard : parent must exist
-                        if ( $parent ) {
-                            $node->saveWithParent( $parent );
-                            $root->fixTree();
-
-                            // set parent for this level
-                            $level = $node->level;
-                            $parents[($level + 1)] = $node;
-                        }
-
-                        $cnt++;
-
-                    } elseif ( true && Impex::isGroupType( $row ) ) {
-                        // OPTION-GROUP,ADDON-GROUP
-                        $parent = $parents[($level + 1)] ?? null;
-
-                        $group = MenuFactory::createMenuItemNode( $menu, $row );
-
-                        if ( $parent ) {
-                            $group->saveWithParent( $parent );
-                            $root->fixTree();
-
-                            // OPTIONS,ADDONS
-                            $cnt++; // move to first available 'option'
-
-                            while ( $cnt < $rows->count() && Impex::isGroupItemType( $rows[$cnt] ) ) {
-                                $item = MenuFactory::createMenuItemNode( $menu, $rows[$cnt], $group );
-                                $cnt++;
-                            }
-                        }
-
-                        $cnt++;
-
-                    } elseif ( true && Impex::isItemType( $row ) ) {
-                        // ITEM,WINE
-                        $parent = $parents[($level + 1)] ?? null;
-
-                        if ( $parent ) {
-                            $item = MenuFactory::createMenuItemNode( $menu, $row, $parent );
-                        }
-                        $cnt++;
-
-                    } else {
-                        // UNKNOWN TYPE
-                        error_log( "Unknown impex row type " . json_encode( $row->toArray() ) );
-                        $cnt++;
-                    }
-                }
-            } );
-        } );
-
-        return true;
-    }
 
     protected function update( \WP_Post $menu, Collection $items ) {
         echo "\nUPDATE: {$menu->post_name}";
