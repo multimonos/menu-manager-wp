92d232a4e0564ee7d4883170694bcc3c4604f8d0
Craig Hopgood <searaig@gmail.com>
2025-04-09 10:50:37 -0400

    update text printer with descriptions

--- lib/MenuManager/Wpcli/TextMenuPrinter.php
+++ lib/MenuManager/Wpcli/TextMenuPrinter.php
@@ -11,10 +11,15 @@ class TextMenuPrinter {
     protected function traverse( $nodes, $prefix = '' ) {
         foreach ( $nodes as $node ) {
             echo "\n"
-                . $node->depth . ' '
+                . str_pad( $node->type, 15 )
+                . str_pad( $node->depth, 3 )
                 . $prefix
                 . ' ' . $node->title
                 . ($node->menuItem->prices ? '  $[' . $node->menuItem->prices . ']' : '');
+
+            if ( $node->description ) {
+                echo "  §[" . substr( $node->description, 0, 20 ) . '...]';
+            }
 
             $this->traverse( $node->children, $prefix . '..' );
         }
