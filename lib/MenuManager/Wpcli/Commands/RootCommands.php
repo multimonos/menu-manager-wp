9f72af350850b9de2136c4d50a6e22b6758f2fae
Craig Hopgood <searaig@gmail.com>
2025-04-12 15:09:19 -0400

    move view command from menu to root

--- lib/MenuManager/Wpcli/Commands/RootCommands.php
+++ lib/MenuManager/Wpcli/Commands/RootCommands.php
@@ -6,6 +6,7 @@ use MenuManager\Actions\ExportAction;
 use MenuManager\Database\db;
 use MenuManager\Database\Model\Node;
 use MenuManager\Database\PostType\MenuPost;
+use MenuManager\Wpcli\TextMenuPrinter;
 use WP_CLI;
 
 class RootCommands {
@@ -51,6 +52,66 @@ class RootCommands {
         }
 
         WP_CLI::success( $rs->getMessage() );
+    }
+
+
+    /**
+     * Print menu to stdout.
+     *
+     * ## OPTIONS
+     *
+     * <id>
+     * : The id or slug of the menu to get.
+     *
+     * [<page>]
+     * : The page to fetch
+     *
+     * ## EXAMPLES
+     *
+     *   wp mm view crowfoot
+     *   wp mm view crowfoot drink
+     *
+     * @when after_wp_load
+     */
+    public function view( $args, $assoc_args ) {
+
+        // @todo refactor into action
+
+        db::load()::connection()->enableQueryLog();
+
+        $id = $args[0];
+        $page = $args[1] ?? null;
+
+        // menu
+        $menu = MenuPost::find( $id );
+
+        if ( ! $menu ) {
+            WP_CLI::error( "Menu not found '{$id}'." );
+        }
+
+        // tree
+        $tree = empty( $page )
+            ? Node::findRootTree( $menu )
+            : Node::findPageTree( $menu, $page );
+
+        if ( ! $tree || $tree->count() === 0 ) {
+            WP_CLI::error( "Menu not found or is empty '" . trim( $id . ' ' . $page ) . "'." );
+        }
+
+
+        // printer
+        $printer = new TextMenuPrinter();
+
+        // print
+        echo "\n$id $page";
+        $printer->print( $tree );
+
+        // log
+        $queries = db::load()::connection()->getQueryLog();
+
+        echo "\n\n";
+        WP_CLI::line( "Nodes: " . $tree->count() );
+        WP_CLI::success( count( $queries ) . ' queries.' );
     }
 
     /**
