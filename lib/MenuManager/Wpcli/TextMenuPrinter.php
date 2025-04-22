b0a45bf8e9b8a88842c8e9e29a5113b06188181a
Craig Hopgood <searaig@gmail.com>
2025-04-22 15:10:28 -0400

    add sort to text printer

--- lib/MenuManager/Wpcli/TextMenuPrinter.php
+++ lib/MenuManager/Wpcli/TextMenuPrinter.php
@@ -7,17 +7,26 @@ use MenuManager\Vendor\Kalnoy\Nestedset\Collection;
 class TextMenuPrinter {
 
     public function print( Collection $tree ): void {
+        echo "\n" . str_pad( 'ID', 8 )
+            . str_pad( 'Sort', 8 )
+            . str_pad( 'Type', 15 )
+            . str_pad( 'De', 5 )
+            . 'Description';
+
         $this->traverse( $tree );
     }
 
     protected function traverse( $nodes, $prefix = '' ) {
+
         foreach ( $nodes as $node ) {
             echo "\n"
-                . str_pad( $node->id, 6 )
-                . str_pad( $node->type, 15 )
-                . str_pad( $node->depth, 3 )
+                . str_pad( $node->id, 8 )
+                . str_pad( $node->sort_order, 8 )
+                . str_pad( $node->type->value, 15 )
+                . str_pad( $node->depth, 5 )
                 . $prefix
                 . ' ' . $node->title
+                . ' ↓[' . $node->sort_order . ']'
                 . ($node->meta->prices ? '  $[' . $node->meta->prices . ']' : '');
 
             if ( $node->meta->tags ) {
